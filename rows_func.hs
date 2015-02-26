84ba83cd6ee455f823ec7c95b62f26871b73f1c2
Notogora <joar.hagg.sylven@telia.com>
2015-02-26 18:40:30 +0100

    This took  way too long

---
+++ rows_func.hs
@@ -0,0 +1,27 @@
+import Data.Char
+import System.IO
+
+
+
+
+
+readfileWrows :: String -> IO ()
+readfileintolist file = do
+  contents <- readFile file
+  do
+    putStrLn $ show (rows_func (words contents) contents [])
+
+
+
+
+rows_func :: [String] -> String ->[String] -> [String]
+
+rows_func [] _ listwrows = reverse listwrows
+
+rows_func wordxs (' ':txtstr) listwrows =
+                              rows_func wordxs txtstr listwrows
+
+rows_func (word:wordxs) txtstr listwrows
+                        | word == take (length word) txtstr =
+                             rows_func wordxs (drop (length word) txtstr) (word:listwrows)
+                        | otherwise = rows_func (word:wordxs) (drop 1 txtstr) ("\n":listwrows)
